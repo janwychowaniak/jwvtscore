61bb907dba110e86ec287a5646d8d7ccc5f2a3b8
Jan Wychowaniak <43786923+janwychowaniak@users.noreply.github.com>
2026-05-12 20:41:52 +0200

    chore: derive package version from metadata

--- src/jwvtscore/__init__.py
+++ src/jwvtscore/__init__.py
@@ -1,5 +1,10 @@
 """jwvtscore package."""
 
+from importlib.metadata import PackageNotFoundError, version
+
 __all__ = ["__version__"]
 
-__version__ = "0.1.0"
+try:
+    __version__ = version("jwvtscore")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
